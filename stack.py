402425b9ebcd9bbd8438e38c301c3f32876923f7
aarthisandhiya <46442329+aarthisandhiya@users.noreply.github.com>
2019-05-20 15:46:56 +0530

    Create stack.py

---
+++ stack.py
@@ -0,0 +1,38 @@
+class Stack:
+
+    def __init__(self):
+        self.stack = []
+
+    def add(self, dataval):
+        if dataval not in self.stack:
+            self.stack.append(dataval)
+            return True
+        else:
+            return False
+    
+    
+    def remove(self):
+        if len(self.stack) <= 0:
+            return ("No element in the Stack")
+        else:
+            return self.stack.pop()
+
+
+
+    def peek(self):     
+	    return self.stack[-1]
+	
+
+
+AStack = Stack()
+AStack.add("Mon")
+AStack.add("Tue")
+AStack.peek()
+print(AStack.peek())
+AStack.add("Wed")
+AStack.add("Thu")
+print(AStack.peek())
+AStack.add("Wed")
+AStack.add("Thu")
+print(AStack.remove())
+print(AStack.remove())
